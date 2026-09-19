e22a689e98ae83b745ae08bc7d5ee76544f485c9
Bagus Primohadi <subs.ptbriska@gmail.com>
2026-09-19 21:25:11 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="id" className="scroll-smooth">
-      <body className="bg-[#0b0f19] text-slate-100 antialiased selection:bg-amber-500 selection:text-slate-950">
+    <html lang="id" className="scroll-smooth bg-[#0b0f19]">
+      <body className="bg-[#0b0f19] text-slate-100 antialiased selection:bg-amber-500 selection:text-slate-950 min-h-screen">
         {children}
       </body>
     </html>
